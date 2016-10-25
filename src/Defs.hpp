1f8db8ce76d22882827f45e534b3c1127a2e26d3
Mrokkk <mrokk@protonmail.com>
2016-10-25 12:35:34 +0200

    Add new colors in the Color enum

--- src/Defs.hpp
+++ src/Defs.hpp
@@ -14,6 +14,15 @@ enum class Color {
     yellow = 33,
     blue = 34,
     magenta = 35,
-    cyan = 36
+    cyan = 36,
+    light_grey = 37,
+    dark_grey = 90,
+    light_red = 91,
+    light_green = 92,
+    light_yellow = 93,
+    light_blue = 94,
+    light_magenta = 95,
+    light_cyan = 96,
+    white = 97
 };
 
