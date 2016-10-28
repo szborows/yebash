0acf59b53d612508427ec8f684aa25a8efc95b37
Mrokkk <mrokk@protonmail.com>
2016-10-28 20:48:21 +0200

    Replace deleted constructor with the explicit keyword

--- src/Printer.hpp
+++ src/Printer.hpp
@@ -7,8 +7,7 @@ namespace yb {
 
 struct Printer {
 
-    Printer() = delete;
-    Printer(std::ostream &output) : output_(output) {}
+    explicit Printer(std::ostream &output) : output_(output) {}
     void print(const char *text, Color color, int offset);
 
 private:
