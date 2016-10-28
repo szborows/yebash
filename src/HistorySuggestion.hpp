794a7c7a5797b1405051aac62962f7bec70eff87
Mrokkk <mrokk@protonmail.com>
2016-10-28 21:09:52 +0200

    Remove already done TODO

--- src/HistorySuggestion.hpp
+++ src/HistorySuggestion.hpp
@@ -4,7 +4,6 @@
 
 namespace yb {
 
-// TODO: move here findCompletion method and history position (from the History class)
 struct HistorySuggestion {
 
     HistorySuggestion(const History &history) : history_(history) { position_ = history_.begin(); }
