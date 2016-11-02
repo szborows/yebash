3344a797978da9163d5f59e72b6dbedd698318fb
Mrokkk <mrokk@protonmail.com>
2016-11-02 22:57:41 +0100

    Fill ArrowHandler with constructor and some fields

--- src/ArrowHandler.hpp
+++ src/ArrowHandler.hpp
@@ -15,7 +15,19 @@ class ArrowHandler {
     const int right_ = 2;
     const int down_ = 3;
 
-    std::array<std::function<void(HistorySuggestion &, Printer &)>, 4> handlers_;
+    using Handler = void(HistorySuggestion &, Printer &);
+    std::array<std::function<Handler>, 4> handlers_;
+    std::array<std::string, 4> escapeCodes = {"\e[1D", "\e[1A", "\e[1C", "\e[1B"};
+    std::array<Char, 4> currentState;
+
+public:
+
+    explicit ArrowHandler(Handler left, Handler up, Handler right, Handler down) {
+        handlers_[left_] = left;
+        handlers_[up_] = up;
+        handlers_[right_] = right;
+        handlers_[down_] = down;
+    } 
 
 };
 
