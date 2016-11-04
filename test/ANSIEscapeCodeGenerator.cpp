2e289bd2aba7f644c08731c6bda8f203a8d1584e
Mrokkk <mrokk@protonmail.com>
2016-11-04 20:08:14 +0100

    Adding next tests

--- test/ANSIEscapeCodeGenerator.cpp
+++ test/ANSIEscapeCodeGenerator.cpp
@@ -21,3 +21,29 @@ TEST_CASE( "can generate proper escape codes for moving forward", "[ANSIEscapeCo
     }
 }
 
+TEST_CASE( "does not generate escape code for moving backward by 0", "[ANSIEscapeCodeGenerator.nullMoveBackward]") {
+    ANSIEscapeCodeGenerator gen;
+    auto result = gen.cursorBackward(0);
+    REQUIRE(result == "");
+}
+
+TEST_CASE( "can generate proper escape codes for moving backward", "[ANSIEscapeCodeGenerator.movingBackward]") {
+    ANSIEscapeCodeGenerator gen;
+    auto test = [&] (int n) {
+        std::string compare{"\033["};
+        compare += std::to_string(n);
+        compare += 'D';
+        auto result = gen.cursorBackward(n);
+        REQUIRE(result == compare); 
+    };
+    for (int i = 1; i < 100; i++) {
+        test(i);
+    }
+}
+
+TEST_CASE( "generates proper escape code for clearing the line", "ANSIEscapeCodeGenerator.clearTerminalLine") {
+    ANSIEscapeCodeGenerator gen;
+    auto result = gen.clearTerminalLine();
+    REQUIRE(result == "\033[K");
+}
+
