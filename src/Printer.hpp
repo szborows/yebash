0d0934199ce92e1620b1945ead1d330a23e35753
Mrokkk <mrokk@protonmail.com>
2016-11-04 18:33:30 +0100

    Add EscapeCodeHandler structs

--- src/Printer.hpp
+++ src/Printer.hpp
@@ -5,6 +5,45 @@
 
 namespace yb {
 
+struct EscapeCodeHandler {
+
+    using EscapeCode = std::string;
+
+    virtual EscapeCode cursorForward(int n);
+    virtual EscapeCode cursorBackward(int n);
+    virtual EscapeCode clearTerminalLine();
+    virtual EscapeCode setColor(Color color);
+};
+
+struct ANSIEscapeCodeHandler : EscapeCodeHandler {
+
+    EscapeCode cursorForward(int n) override {
+        std::string a{"\e["};
+        a += n;
+        a += 'C';
+        return a;
+    }
+
+    EscapeCode cursorBackward(int n) override {
+        std::string a{"\e["};
+        a += n;
+        a += 'D';
+        return a;
+    }
+
+    EscapeCode clearTerminalLine() override {
+        return EscapeCode{"\e[K"};
+    }
+
+    EscapeCode setColor(Color color) override {
+        EscapeCode a{"\e["};
+        a += static_cast<int>(color);
+        a += 'm';
+        return a;
+    }
+
+};
+
 struct Printer {
 
     explicit Printer(std::ostream &output) : output_(output) {}
