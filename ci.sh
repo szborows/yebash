18814c816d10f092cab58f47234875dfaeca407c
Mrokkk <mrokk@protonmail.com>
2016-11-07 21:01:03 +0100

    Add valgrind job and proceed with some CI and build system improvements

---
+++ ci.sh
@@ -0,0 +1,24 @@
+set -e
+
+job=$1
+export CXX=$COMPILER
+
+# TODO remove this after fixing yebash
+if [ ! -f ~/.bash_history ]; then
+    touch ~/.bash_history
+fi
+
+mkdir -p build
+cd build
+
+cores=$(nproc)
+
+case "$job" in
+    "valgrind")
+        cmake ..
+        make tests-valgrind -j$cores ;;
+    "coverage")
+        cmake .. -DCOVERAGE=ON
+        make tests-cov -j$cores ;;
+esac
+
